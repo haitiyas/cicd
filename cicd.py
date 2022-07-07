cfafea4aeeafaad6265f6dd4f4da00a9b8c187a5
haitiyas <tiyas.saengyeppeo@gmail.com>
2022-07-07 14:26:16 +0700

    testing

--- cicd.py
+++ cicd.py
@@ -1,8 +1,12 @@
 import requests
+from datetime import datetime
 
 print("hello world")
 print("testing CI/CD")
 
 response = requests.get("https://www.google.com")
 
-print(response.text)
+waktu = datetime.now()
+
+with open("tempResponse/"+str(waktu)+".txt","w") as f:
+    f.write(response.text)
